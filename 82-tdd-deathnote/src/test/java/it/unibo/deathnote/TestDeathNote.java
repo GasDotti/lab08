698fa9cb136ea6e5f9285e76254431ae05a9ca66
Castorri Lorenzo <loricastorri@gmail.com>
2025-11-10 18:36:42 +0100

    82: test for the writing of the name. Failing for missing implementation.

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,8 +1,11 @@
 package it.unibo.deathnote;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +18,12 @@ import it.unibo.deathnote.impl.DeathNoteImpl;
 
 class TestDeathNote {
 
-    static private final String EMPTY_STRING = "";
+    private static final String EMPTY_STRING = "";
     private DeathNote book;
     private final int[] falseRules = {0, DeathNote.RULES.size() + 1};
     private List<String> names;
+    // List with a null element.
+    private List<Object> falseNames;
 
     /**
      * Configuration for the tests.
@@ -27,26 +32,50 @@ class TestDeathNote {
     void setup() {
         book = new DeathNoteImpl();
         names = new ArrayList<>();
-        names.addAll(List.of("pippo", "pluto", "paperino", "giorgia"));
+        names.addAll(List.of("pippo", "pluto", "paperino", "giorgia", ""));
+        falseNames = new ArrayList<>();
     }
 
     /**
-     * Checks if the selection of the rule is correct
+     * Checks if the selection of the rule is correct.
      */
     @Test
     void testRule() {
         final int validRule = 1;
 
+        // Testing illegal arguments.
         for (final int i : falseRules) {
             try {
                 book.getRule(i);
-            } catch (final IllegalArgumentException ex) {
+            } catch (final IllegalArgumentException e) {
                 assertEquals(DeathNote.RULES.get(validRule - 1), book.getRule(validRule));
             }
         }
+        // Testing content of the Rules.
         for (int i = 1; i <= DeathNote.RULES.size(); i++) {
             assertNotEquals(book.getRule(i), EMPTY_STRING);
             assertNotNull(book.getRule(i));
+        }
+    }
+
+    /**
+     * Tries the correct functioning for the action of "writing a name".
+     */
+    @Test
+    void testWriteName() {
+        for (final var n : List.of(names, falseNames)) {
+            assertTrue(book.isNameWritten(n.toString()));
+
+            try {
+                book.writeName(n.toString());
+                assertTrue(book.isNameWritten(n.toString()));
+
+                // non ho capito il punto '* verify that another human has not been written in the notebook'
+
+                assertFalse(book.isNameWritten(EMPTY_STRING));
+            } catch (final NullPointerException e) {
+                assertNull(n);
+            }
         }
     }
 }
