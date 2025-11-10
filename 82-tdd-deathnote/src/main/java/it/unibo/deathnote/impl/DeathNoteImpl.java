c5c6cf11c5dbbbe71befaf9de730821a6ffff948
Castorri Lorenzo <loricastorri@gmail.com>
2025-11-10 12:10:36 +0100

    82: file internal setup. Minimal number of errors during build.

---
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -0,0 +1,84 @@
+package it.unibo.deathnote.impl;
+
+import it.unibo.deathnote.api.DeathNote;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class that implements {@link DeathNote.java}.
+ */
+public class DeathNoteImpl implements DeathNote {
+
+    private final List<String> content; 
+
+    /**
+     * Base constructor, it initializes an empty DeathNote.
+     */
+    public DeathNoteImpl() {
+        this.content = new ArrayList<>();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getRule(final int ruleNumber) {
+        //TODO
+        throw new IllegalArgumentException();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void writeName(final String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeDeathName'");
+    }
+
+    /**
+     *  {@inheritDoc}
+     */
+    @Override
+    public boolean writeDeathCause(final String cause) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeDeathCause'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean writeDetails(final String details) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeDetails'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getDeathCause(final String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDeathCause'");
+    }
+
+    /**
+     * {@inheritDoc} 
+     */
+    @Override
+    public String getDeathDetails(final String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDeathDetails'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isNameWritten(final String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isNameWritten'");
+    }
+}
